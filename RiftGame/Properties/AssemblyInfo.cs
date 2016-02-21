a21661d8476f468983b604b9e1b40e725358ea3f
MHDante <dcamaren@ryerson.ca>
2016-02-21 08:35:30 -0500

    Now the pain starts. First Compiling build.

--- RiftGame/Properties/AssemblyInfo.cs
+++ RiftGame/Properties/AssemblyInfo.cs
@@ -5,12 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RiftGame")]
+[assembly: AssemblyTitle("OrbItProcs")]
+[assembly: AssemblyProduct("OrbItProcs")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RiftGame")]
-[assembly: AssemblyCopyright("Copyright © Guy Godin 2014")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
