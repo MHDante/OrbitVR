7d55c59db988a5c2727a48acc4f39b3c764069bd
MHDante <dcamaren@ryerson.ca>
2016-02-23 07:27:44 -0500

    This is a drug.

--- UI/UserInterface.cs
+++ UI/UserInterface.cs
@@ -7,22 +7,30 @@ using SharpDX.Toolkit;
 
 namespace OrbitVR.UI {
   public class UserInterface {
-    public enum Selection {
-      PlaceNode,
-      TargetSelection,
-      GroupSelection,
-      RandomNode,
-    }
-    
-    public static Vector2 MousePos;
+
+    private static Vector2 _mousePos;
     public static Vector2 WorldMousePos;
-    public Action<int> ScrollAction;
-    public float zoomfactor { get; set; }
+    public static KeyboardState keybState, oldKeyBState;
+    public static MouseState mouseState, oldMouseState;
+    int _oldMouseScrollValue;
+    public Action<int> ScrollAction { get; }
+    private float Zoomfactor { get; }
     public bool IsPaused { get; set; }
 
-    public UserInterface() {
+    public OrbIt game {
+      get { return OrbIt.Game; }
+    }
 
-      zoomfactor = 0.9f;
+    public Room room {
+      get { return game.Room; }
+    }
+
+
+    public KeyManager keyManager { get; set; }
+
+    public UserInterface() {
+      ScrollAction = null;
+      Zoomfactor = 0.9f;
       IsPaused = false;
       keyManager = new KeyManager(this);
     }
@@ -36,30 +44,14 @@ namespace OrbitVR.UI {
 
     public void ProcessKeyboard() {
       keybState = Keyboard.GetState();
-
-      if (keybState.IsKeyDown(Keys.Y))
-        hovertargetting = true;
-      else
-        hovertargetting = false;
-
+      
 
       if (keybState.IsKeyDown(Keys.Space) && oldKeyBState.IsKeyUp(Keys.Space)) {
         room.Update(null);
       }
+      
 
-      if (keybState.IsKeyDown(Keys.LeftShift)) {
-        if (!isShiftDown) {
-          MouseState ms = Mouse.GetState();
-          spawnPos = new Vector2(ms.X, ms.Y)/room.Camera.zoom;
-        }
-        isShiftDown = true;
-      }
-      else {
-        isShiftDown = false;
-      }
-
-      //if (keybState.IsKeyDown(Keys.F) && !oldKeyBState.IsKeyDown(Keys.F))
-      //    IsPaused = !IsPaused;
+      if (keybState.IsKeyDown(Keys.F) && !oldKeyBState.IsKeyDown(Keys.F)) IsPaused = !IsPaused;
 
       oldKeyBState = Keyboard.GetState();
     }
@@ -89,15 +81,15 @@ namespace OrbitVR.UI {
 
     public void ProcessMouse() {
       mouseState = Mouse.GetState();
-      
+
       //if (mouseState.XButton1 == ButtonState.Pressed)
       //    System.Console.WriteLine("X1");
       //
       //if (mouseState.XButton2 == ButtonState.Pressed)
       //    System.Console.WriteLine("X2");
 
-      MousePos = new Vector2(mouseState.X, mouseState.Y) - OrbIt.Game.Room.Camera.CameraOffsetVect;
-      WorldMousePos = (MousePos/room.Camera.zoom) + room.Camera.virtualTopLeft;
+      _mousePos = new Vector2(mouseState.X, mouseState.Y) - OrbIt.Game.Room.Camera.CameraOffsetVect;
+      WorldMousePos = (_mousePos/room.Camera.zoom) + room.Camera.virtualTopLeft;
       //ignore mouse clicks outside window
       if (!IsPaused) {
         if (mouseState.X >= OrbIt.ScreenWidth || mouseState.X < 0 || mouseState.Y >= OrbIt.ScreenHeight ||
@@ -125,13 +117,12 @@ namespace OrbitVR.UI {
       int worldMouseX = (int) WorldMousePos.X;
       int worldMouseY = (int) WorldMousePos.Y;
 
-
-      if (hovertargetting) {
+      
         if (true) // || mouseState.LeftButton == ButtonState.Pressed)
         {
           bool found = false;
           for (int i = room.MasterGroup.fullSet.Count - 1; i >= 0; i--) {
-            Node n = (Node) room.MasterGroup.fullSet.ElementAt(i);
+            Node n = room.MasterGroup.fullSet.ElementAt(i);
             // find node that has been clicked, starting from the most recently placed nodes
             if (Vector2.DistanceSquared(n.body.pos, new Vector2(worldMouseX, worldMouseY)) < n.body.radius*n.body.radius) {
               room.TargetNode = n;
@@ -141,51 +132,16 @@ namespace OrbitVR.UI {
           }
           if (!found) room.TargetNode = null;
         }
+
+      if (mouseState.ScrollWheelValue < _oldMouseScrollValue) {
+        room.Camera.zoom *= Zoomfactor;
+      }
+      else if (mouseState.ScrollWheelValue > _oldMouseScrollValue) {
+        room.Camera.zoom /= Zoomfactor;
       }
 
-      if (mouseState.ScrollWheelValue < oldMouseScrollValue) {
-        room.Camera.zoom *= zoomfactor;
-      }
-      else if (mouseState.ScrollWheelValue > oldMouseScrollValue) {
-        room.Camera.zoom /= zoomfactor;
-      }
-
-      oldMouseScrollValue = mouseState.ScrollWheelValue;
+      _oldMouseScrollValue = mouseState.ScrollWheelValue;
       oldMouseState = mouseState;
     }
-    
-
-    #region /// Fields ///
-
-    public OrbIt game {
-      get { return OrbIt.Game; }
-    }
-
-    public Room room {
-      get { return game.Room; }
-    }
-
-
-    public KeyManager keyManager { get; set; }
-
-
-    public static KeyboardState keybState, oldKeyBState;
-    public static MouseState mouseState, oldMouseState;
-
-    //public string currentSelection = "placeNode";//
-    public Selection currentSelection = Selection.PlaceNode;
-    int oldMouseScrollValue = 0; //
-    bool hovertargetting = false; //
-    //int rightClickCount = 0;//
-    //int rightClickMax = 1;//
-    public int sWidth = 1000; ////
-    public int sHeight = 600; ////
-    bool isShiftDown = false;
-    //bool isTargeting = false;
-    public Vector2 spawnPos;
-    Vector2 groupSelectionBoxOrigin = new Vector2(0, 0);
-    public HashSet<Node> groupSelectSet;
-
-    #endregion
   }
 }
